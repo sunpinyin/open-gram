4d8bb45a74d99b6d33745f03b83129871ea9eab1
Casper Ti. Vector <CasperVector@gmail.com>
2013-12-11 01:35:11 +0800

    Improve readability of *extend.py.
    * Remove pre-mature optimisation.
    * Real modularisation.
    * Minor improvements.

--- tools/utils/sunpinyin-hanzi-table-extend.py
+++ tools/utils/sunpinyin-hanzi-table-extend.py
@@ -1,28 +1,27 @@
-#!/usr/bin/python3 -OO
+#!/usr/bin/python3
 # vim:noet:ts=4:sw=4
 
-import re
 import sys
-from sunpinyin_char_extend import dictPy
+from sunpinyin_char_extend import char_dict_gen
 
-dictInput = "/dev/stdin" if sys.argv[1] == '-' else sys.argv[1]
-dictOutput = "/dev/stdout" if sys.argv[2] == '-' else sys.argv[2]
+dictPy = char_dict_gen(
+	"Unihan_Readings.txt" if len(sys.argv) < 2 else sys.argv[1]
+)
 
 def fmt_chr_info(c):
-	return ' '.join([c] + [py for py in dictPy[c]])
+	return " ".join([c] + [py for py in dictPy[c]])
 
-patcMinus = re.compile(r"-")
-with open(dictOutput, "w") as outputFile:
-	for l in open(dictInput):
-		if len(l) == 0:
-			continue
-		c = l.split()[0]
-		if len(c) == 1 and c in dictPy and not patcMinus.search(l):
-			outputFile.write(fmt_chr_info(c) + "\n")
-		else:
-			outputFile.write(l)
-		if c in dictPy:
-			dictPy.pop(c)
-	for c in dictPy:
-		outputFile.write(fmt_chr_info(c) + "\n")
+for l in sys.stdin:
+	if len(l) == 0:
+		continue
+	c = l.split()[0]
+	if len(c) == 1 and c in dictPy and not "-" in l:
+		sys.stdout.write(fmt_chr_info(c) + "\n")
+	else:
+		sys.stdout.write(l)
+	if c in dictPy:
+		dictPy.pop(c)
+
+for c in dictPy:
+	sys.stdout.write(fmt_chr_info(c) + "\n")
 
--- tools/utils/sunpinyin_char_extend.py
+++ tools/utils/sunpinyin_char_extend.py
@@ -1,20 +1,16 @@
-#!/usr/bin/python3 -OO
+#!/usr/bin/python3
 # vim:noet:ts=4:sw=4
 # coding=UTF-8
 
 import re
 
-unihanReadings = "Unihan_Readings.txt"
-dictPy = {}
-
-## Functions to process Unihan data.
+# Functions to process Unihan data.
 
 def codepoint_to_chr(s):
-	patcCodeptHdr = re.compile(r"^U\+")
-	return chr(int(patcCodeptHdr.sub("", s), 16))
+	return chr(int(re.sub(r"^U\+", "", s), 16))
 
 def get_unihan_properties(prop, fname):
-	patLine = '\t'.join([r"(U\+[0-9A-F]+)", "(" + prop + ")", "([^\t]+)"])
+	patLine = "\t".join([r"(U\+[0-9A-F]+)", "(" + prop + ")", "([^\t]+)"])
 	patcLine = re.compile("^" + patLine + "$")
 	return map(
 		(lambda ss: (codepoint_to_chr(ss[0]), ss[2])),
@@ -24,13 +20,7 @@ def get_unihan_properties(prop, fname):
 		)]
 	)
 
-def add_entry_info_to_dict(prop, py_proc):
-	for (c, rawPys) in get_unihan_properties(prop, unihanReadings):
-		if not c in dictPy and ord(c) < 0x20000:
-			dictPy[c] = set()
-			dictPy[c].update(py_proc(rawPys))
-
-## Functions to process pinyin strings.
+# Functions to process pinyin strings.
 
 def py_translate(py):
 	dictPyTrans = str.maketrans(
@@ -39,30 +29,38 @@ def py_translate(py):
 	return py.translate(dictPyTrans)
 
 def py_utov(py):
-	patcUtoV = re.compile(r"([ln])[ǖǘǚǜü]")
-	return patcUtoV.sub(r"\1v", py)
+	return re.sub(r"([ln])[ǖǘǚǜü]", r"\1v", py)
 
 def py_rm_tone(py):
-	patcPyTone = re.compile(r"[1-5]$")
-	return patcPyTone.sub("", py)
+	return re.sub(r"[1-5]$", "", py)
 
-## Process related Unihan entries.
+# Process related Unihan entries.
 
 def py_proc_kHanyuPinlu(pys):
-	patcKHanyuPinlu = re.compile(r"([^0-9]+[1-5])\(([0-9]+)\)")
-	return [py_utov(py_rm_tone(patcKHanyuPinlu.match(py).group(1)))
-		for py in pys.split()]
-add_entry_info_to_dict("kHanyuPinlu", py_proc_kHanyuPinlu)
+	return [py_utov(py_rm_tone(
+		re.match(r"([^0-9]+[1-5])\(([0-9]+)\)", py).group(1)
+	)) for py in pys.split()]
 
 def py_proc_kXHC1983(pys):
-	patcKHanyuPinyin = re.compile(r"(?<=:).*")
-	return [py_translate(py_utov(patcKHanyuPinyin.search(py).group(0)))
+	return [py_translate(py_utov(re.search(r"(?<=:).*", py).group(0)))
 		for py in pys.split()]
-add_entry_info_to_dict("kXHC1983", py_proc_kXHC1983)
 
 def py_proc_kHanyuPinyin(pyss):
-	patcKHanyuPinyin = re.compile(r"(?<=:).*")
 	return [py_translate(py_utov(py)) for pys in pyss.split()
-		for py in patcKHanyuPinyin.search(pys).group(0).split(',')]
-add_entry_info_to_dict("kHanyuPinyin", py_proc_kHanyuPinyin)
+		for py in re.search("(?<=:).*", pys).group(0).split(",")]
+
+def add_entry_info_to_dict(dictPy, unihanReadings, prop, py_proc):
+	for (c, rawPys) in get_unihan_properties(prop, unihanReadings):
+		if not c in dictPy and ord(c) < 0x20000:
+			dictPy[c] = set()
+			dictPy[c].update(py_proc(rawPys))
+
+def char_dict_gen(unihanReadings):
+	dictPy = {}
+	[add_entry_info_to_dict(dictPy, unihanReadings, *args) for args in [
+		("kHanyuPinlu", py_proc_kHanyuPinlu),
+		("kXHC1983", py_proc_kXHC1983),
+		("kHanyuPinyin", py_proc_kHanyuPinyin)
+	]]
+	return dictPy
 
