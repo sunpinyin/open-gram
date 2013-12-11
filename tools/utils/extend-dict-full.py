1a28d66ccc99e585765825aeb486fb1251dc8caa
Casper Ti. Vector <CasperVector@gmail.com>
2013-12-11 22:40:06 +0800

    Use consistent file name for scripts in tools/utils/.

--- tools/utils/extend-dict-full.py
+++ tools/utils/extend-dict-full.py
@@ -2,28 +2,26 @@
 # vim:noet:ts=4:sw=4
 
 import sys
-import sunpinyin_char_extend
+import extend_charmap
 
-dictPy = sunpinyin_char_extend.char_dict_gen(
+dictPy = extend_charmap.char_dict_gen(
 	"Unihan_Readings.txt" if len(sys.argv) < 2 else sys.argv[1]
 )
 
-def fmt_chr_info(c, idx):
-	return " ".join([c, str(idx)] + sorted([py for py in dictPy[c]]))
+def fmt_chr_info(c):
+	return " ".join([c] + sorted([py for py in dictPy[c]]))
 
 for l in sys.stdin:
 	if len(l) == 0:
 		continue
 	c = l.split()[0]
-	idx = int(l.split()[1])
 	if len(c) == 1 and c in dictPy and not "%" in l:
-		sys.stdout.write(fmt_chr_info(c, idx) + "\n")
+		sys.stdout.write(fmt_chr_info(c) + "\n")
 	else:
 		sys.stdout.write(l)
 	if c in dictPy:
 		dictPy.pop(c)
 
 for c in dictPy:
-	idx += 1
-	sys.stdout.write(fmt_chr_info(c, idx) + "\n")
+	sys.stdout.write(fmt_chr_info(c) + "\n")
 
--- tools/utils/extend-hanzi-table.py
+++ tools/utils/extend-hanzi-table.py
@@ -2,9 +2,9 @@
 # vim:noet:ts=4:sw=4
 
 import sys
-import sunpinyin_char_extend
+import extend_charmap
 
-dictPy = sunpinyin_char_extend.char_dict_gen(
+dictPy = extend_charmap.char_dict_gen(
 	"Unihan_Readings.txt" if len(sys.argv) < 2 else sys.argv[1]
 )
 
