267887980e7534bac6f6c8893ecb837e0a71a928
Casper Ti. Vector <CasperVector@gmail.com>
2013-12-13 23:32:21 +0800

    Generate dict.utf8 from dict.full.

---
+++ tools/utils/gen-dict-utf8.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# vim:noet:ts=4:sw=4
+
+import sys
+
+dictHead = "../../lexicon/dict_head.utf8" if len(sys.argv) < 2 else sys.argv[1]
+
+for l in open(dictHead):
+	sys.stdout.write(l)
+
+i = 100
+for l in sys.stdin:
+	fields = l.strip().split()
+	fields = fields[0 : 1] + [str(i)] + fields[1 :]
+	sys.stdout.write(" ".join(fields) + "\n")
+
