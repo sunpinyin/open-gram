e220ddc9a9cb5ad7e8ab332b4f946ef5cd66716b
Casper Ti. Vector <CasperVector@gmail.com>
2014-08-20 15:15:44 +0800

    sort-fields.py: no longer process dict.utf8 directly since it's generated.

--- tools/utils/sort-fields.py
+++ tools/utils/sort-fields.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 # vim:noet:ts=4:sw=4
 
-# Sort pinyins in `hanzi_table.utf8", `dict.full" and `dict.utf8" in alphabet
-# order.
+# Sort pinyins in `hanzi_table.utf8' and `dict.full' in alphabet order.
 #
 # Q: Why bother?
 # A: We will benefit from this when we apply mass modifications to pinyin"s
@@ -10,11 +9,7 @@
 #	entries that actually changed ONLY (instead of mixed with changes like
 #	that from `唵 n ng an" to `唵 an n ng").
 
-import os
 import sys
-
-# 1 for `hanzi_table.utf8" and `dict.full"; 2 for `dict.utf8".
-heads = int(sys.argv[1]) if len(sys.argv) > 1 else 1
 
 for line in sys.stdin:
 	words = line.strip().split()
